f02c7f7f823f35e282e68b84969aa906eb50efe8
Dane Miluski <dmiluski@gmail.com>
2020-07-29 20:16:50 -0700

    Carthage Support
    Problem:
    Existing Firebase integrations do not support Carthage
    
    Approach:
    Including packaging project -> framework which can be consumed by developers
    
    Changes:
    + Include Cartfile
    + Include Packaging Project
    - Ignore unnecessary files
    + Expose headers
    + Remove Code Signing
    + Share Scheme
    + Public Header Imports

---
+++ Segment-Intercom/Resources/Segment_Intercom.h
@@ -0,0 +1,19 @@
+//
+//  Segment_Intercom.h
+//  Segment-Intercom
+//
+//  Created by Dane Miluski on 7/29/20.
+//  Copyright © 2020 Mapbox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Segment_Intercom.
+FOUNDATION_EXPORT double Segment_IntercomVersionNumber;
+
+//! Project version string for Segment_Intercoms.
+FOUNDATION_EXPORT const unsigned char Segment_IntercomVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Segment_Intercom/PublicHeader.h>
+#import <Segment_Intercom/SEGIntercomIntegration.h>
+#import <Segment_Intercom/SEGIntercomIntegrationFactory.h>

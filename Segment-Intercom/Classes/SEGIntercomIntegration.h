fb826f396c7ae7591b77db8464adb478809c4cfa
Brie McNally <31782219+briemcnally@users.noreply.github.com>
2020-10-07 14:54:26 -0600

    [STRATCONN-419] Update namespacing for iOS14  (#12)
    * Update namespacing
    
    * Update simulator
    
    * Update conditionals
    
    * Update pods
    
    * Update circle config
    
    * Update circle config

--- Segment-Intercom/Classes/SEGIntercomIntegration.h
+++ Segment-Intercom/Classes/SEGIntercomIntegration.h
@@ -8,7 +8,11 @@
 //
 
 #import <Foundation/Foundation.h>
+#if defined(__has_include) && __has_include(<Analytics/SEGAnalytics.h>)
 #import <Analytics/SEGIntegration.h>
+#else
+#import <Segment/SEGIntegration.h>
+#endif
 #import <Intercom/Intercom.h>
 
 
